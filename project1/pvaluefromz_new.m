88391dd7b8ce39b025743e35271b79809a6334cf
sethspringer <90924994+sethspringer@users.noreply.github.com>
2022-05-09 12:50:32 -0500

    changes.......

--- project1/pvaluefromz_new.m
+++ project1/pvaluefromz_new.m
@@ -9,9 +9,7 @@ function [pvalue] = pvaluefromz_new(z)
     z = abs(z);
     F = @(x)(exp (-0.5*(x.^2))./sqrt (2*pi));
     p = integral(F, z, 100);
-    
-    %Commenting this out bc it is unnecessary
-    %fprintf ('\nOne tail p-value : %1.6f', p);
+
 
     %fprintf ('\nTwo tail p-value : %1.6f\n' ,p*2)
 
